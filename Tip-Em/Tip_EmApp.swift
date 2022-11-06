84d4e4da18b53f4b4b77c1be3f5e5de5103c100c
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2022-11-06 16:54:55 -0500

    Initial Commit

---
+++ Tip-Em/Tip_EmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_EmApp.swift
+//  Tip-Em
+//
+//  Created by Adrien CHABAUD on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Tip_EmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
