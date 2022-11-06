986dfa67fd9a5f9e72267a7b81e39a638289a2c6
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2022-11-06 17:50:05 -0500

    Adding background + Begin UI

--- Tip-Em/ContentView.swift
+++ Tip-Em/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            // Background
+            Rectangle()
+                .foregroundColor(Color(red: 243/255, green: 154/255, blue: 54/255))
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                .frame(width: 550)
+                .foregroundColor(.white)
+                .rotationEffect(Angle(degrees: 80))
+            VStack {
+                HStack {
+                    //Title
+                    Text("Tip-Em")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .padding(.trailing, 30)
+                    
+                    // Share button (to send by text)
+                    Image(systemName: "square.and.arrow.up")
+                        .foregroundColor(.black)
+                }.scaleEffect(2)
+                    .padding(.leading, 150)
+                    .padding(.trailing, 30)
+                    .padding([.top,	 .bottom], 16)
+                
+                VStack {
+                    // Bill amount
+                    Text("Enter bill amount:")
+                        .foregroundColor(Color(red: 159/255, green: 166/255, blue: 162/255))
+                    HStack {
+                        Text("$")
+                        Text("112.63")
+                    }
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
